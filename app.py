fe2c294d4499413bb2476500bcc8f4d00f8c1f7c
Asad Ali Panhwar <asadalipuh5@gmail.com>
2024-10-11 23:43:41 +0500

    HTML file uploaded

--- app.py
+++ app.py
@@ -11,3 +11,7 @@ app = application
 def index():
     return render_template('index.html')
 
+@app.route('/predictdata', methods = ['GET', 'POST'])
+def predict_datapoint():
+    if request.method=='GET':
+        return render_template('home.html')
